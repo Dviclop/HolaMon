d3c759413bc8d86eb3e8b7b3d2ed0c82002c9b95
tarda <tarda@localhost>
2015-12-01 19:39:29 +0100

    afegit Dani

--- HelloWorld/src/helloworld/HelloWorld.java
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -16,7 +16,7 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
-        // TODO code application logic here
+        System.out.println("tu madre :D ");
     }
     
 }
